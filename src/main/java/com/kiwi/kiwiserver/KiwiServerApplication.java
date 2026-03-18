a9087a7faccbe49ecb41723dea20eaa03c7eccd0
ohyerim025-alt <ohyerim025@gmail.com>
2026-03-19 01:13:04 +0900

    first commit

---
+++ src/main/java/com/kiwi/kiwiserver/KiwiServerApplication.java
@@ -0,0 +1,13 @@
+package com.kiwi.kiwiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiwiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KiwiServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kiwi/kiwiserver/KiwiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiwi.kiwiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiwiServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
