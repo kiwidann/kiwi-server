2ff47f6dedc1020353987a6d37a938aa99f56288
ohyerim025-alt <ohyerim025@gmail.com>
2026-06-07 13:29:34 +0900

    :bug: fix: Swagger 서버 URL 설정

--- src/main/java/com/kiwi/kiwiserver/global/config/SwaggerConfig.java
+++ src/main/java/com/kiwi/kiwiserver/global/config/SwaggerConfig.java
@@ -1,11 +1,15 @@
 package com.kiwi.kiwiserver.global.config;
+
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -15,6 +19,9 @@ public class SwaggerConfig {
         String jwtSchemeName = "bearerAuth";
 
         return new OpenAPI()
+                .servers(List.of(
+                        new Server().url("https://api.kiwi-server.uk").description("Production Server")
+                ))
                 .info(new Info()
                         .title("Kiwi API")
                         .version("v1")
